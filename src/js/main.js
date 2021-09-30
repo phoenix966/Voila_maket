0d530e8dafeefef744729a530ebd2df4efad4e81
Uri <stranger354999@gmail.com>
2021-09-30 17:22:44 +0600

    start project

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+//=require ../../node_modules/jquery/dist/jquery.js
+//=require ../../node_modules/bootstrap/dist/js/bootstrap.js
+
+const foo = () =>{
+    console.log(fgfg);
+    console.log(rt);
+}
+
