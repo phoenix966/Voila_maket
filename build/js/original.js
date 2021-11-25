66f1323409da7372f0e608a008907d227caf48b3
Uri <stranger354999@gmail.com>
2021-11-25 13:33:45 +0600

    edit for deploy

---
+++ build/js/original.js
@@ -0,0 +1,12 @@
+
+  let hamburger = document.querySelector(".hamburger");
+  let mobile = document.querySelector(".mobile");
+
+  hamburger.addEventListener("click", function() {
+    hamburger.classList.toggle("is-active");
+    mobile.classList.toggle('mobile--hide');
+  });
+
+
+
+
